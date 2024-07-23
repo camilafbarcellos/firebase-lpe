0543d163dcf622f22c5967cc92dbb579d4ecd939
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-07-23 10:26:19 -0300

    feat: add posts service to posts screen

--- src/componentes/telas/posts/Posts.jsx
+++ src/componentes/telas/posts/Posts.jsx
@@ -3,22 +3,23 @@ import PostsContext from "./PostsContext";
 import Tabela from "./Tabela";
 import Form from "./Form";
 import Carregando from "../../comuns/Carregando";
-
+import { auth } from '../../../firebaseConfig';
+import { useAuthState } from "react-firebase-hooks/auth";
+import {
+    deletePostFirebase, addPostFirebase,
+    updatePostFirebase, getPostsUIDFirebase
+} from '../../servicos/PostsService';
+import { Navigate } from "react-router-dom";
 
 function Posts() {
 
+    const [user, loading, error] = useAuthState(auth);
     const [alerta, setAlerta] = useState({ status: "", message: "" });
     const [listaObjetos, setListaObjetos] = useState([]);
     const [editar, setEditar] = useState(false);
     const [objeto, setObjeto] = useState({
-        id: '',
-        titulo: '',
-        texto: '',
-        url: '',
-        tipo: '',
-        usuario: '',
-        email: '',
-        uid: ''
+        id: "", titulo: "", texto: "", tipo: "", url: "",
+        uid: user?.uid, usuario: user?.displayName, email: user?.email
     });
     const [carregando, setCarregando] = useState(true);
     const [abreDialogo, setAbreDialogo] = useState(false);
@@ -27,14 +28,8 @@ function Posts() {
         setEditar(false);
         setAlerta({ status: "", message: "" });
         setObjeto({
-            id: '',
-            titulo: '',
-            texto: '',
-            url: '',
-            tipo: '',
-            usuario: '',
-            email: '',
-            uid: ''
+            id: "", titulo: "", texto: "", tipo: "", url: "",
+            uid: user?.uid, usuario: user?.displayName, email: user?.email
         });
         setAbreDialogo(true)
     }
@@ -49,19 +44,29 @@ function Posts() {
     const acaoCadastrar = async e => {
         e.preventDefault();
         if (editar) {
-
             try {
-
-                setAlerta({ status: "success", message: "Post atualizado com sucesso" });
+                await updatePostFirebase(objeto);
+                setAlerta({
+                    status: "success", message: "Post atualizado com sucesso"
+                });
             } catch (err) {
-                setAlerta({ status: "error", message: "Erro ao atualizar o POST:" + err });
+                setAlerta({
+                    status: "error", message: "Erro ao atualizar o POST:"
+                        + err
+                });
             }
-        } else { // novo 
+        } else { // novo
             try {
+                setObjeto(await addPostFirebase(objeto));
                 setEditar(true);
-                setAlerta({ status: "success", message: "Post criado com sucesso" });
+                setAlerta({
+                    status: "success", message: "Post criado com sucesso"
+                });
             } catch (err) {
-                setAlerta({ status: "error", message: "Erro ao criar o POST:" + err });
+                setAlerta({
+                    status: "error", message: "Erro ao criar o POST:" +
+                        err
+                });
             }
         }
     }
@@ -75,55 +80,47 @@ function Posts() {
     const remover = async (objeto) => {
         if (window.confirm("Remover este objeto?")) {
             try {
-                setAlerta({ status: "success", message: "Post removido com sucesso!" });
+                deletePostFirebase(objeto);
+                setAlerta({
+                    status: "success", message: "Post removido com sucesso!"
+                });
             } catch (err) {
-                setAlerta({ status: "error", message: "Erro ao  remover: " + err });
+                setAlerta({
+                    status: "error", message: "Erro ao remover: " + err
+                });
             }
         }
     }
 
     useEffect(() => {
-        setListaObjetos([
-            {
-                id: '1xxx',
-                titulo: 'Usando Firebase',
-                texto: 'Como usar o Firebase',
-                url: 'https://firebase.google.com/docs/guides?hl=pt-br',
-                tipo: 'Documentação',
-                usuario: 'Jorge',
-                email: 'jlbavaresco@gmail.com',
-                uid: 'jj123'
-            },
-            {
-                id: '2xxx',
-                titulo: 'Analysis of Component Libraries for React JS',
-                texto: 'Analysis of Component Libraries for React JS',
-                url: 'https://iarjset.com/wp-content/uploads/2021/06/IARJSET.2021.8607.pdf',
-                tipo: 'Artigo',
-                usuario: 'Jorge',
-                email: 'jlbavaresco@gmail.com',
-                uid: 'jj123'
-            }
-        ])
+        setCarregando(true);
+        if (user?.uid != null) {
+            const uid = user?.uid;
+            getPostsUIDFirebase(uid, setListaObjetos);
+        }
         setCarregando(false);
-    }, []);
+    }, [user]);
 
-    return (
-        <PostsContext.Provider value={{
-            alerta, setAlerta,
-            listaObjetos, setListaObjetos,
-            remover,
-            objeto, setObjeto,
-            editarObjeto, novoObjeto, acaoCadastrar,
-            handleChange, abreDialogo, setAbreDialogo
-        }}>
-            <Carregando carregando={carregando}>
-                <Tabela />
-            </Carregando>
-            <Form />
-        </PostsContext.Provider>
-    )
+    if (user) {
+        return (
+            <PostsContext.Provider value={{
+                alerta, setAlerta,
 
+                listaObjetos, setListaObjetos,
+                remover,
+                objeto, setObjeto,
+                editarObjeto, novoObjeto, acaoCadastrar,
+                handleChange, abreDialogo, setAbreDialogo
+            }}>
+                <Carregando carregando={carregando}>
+                    <Tabela />
+                </Carregando>
+                <Form />
+            </PostsContext.Provider>
+        )
+    } else {
+        return <Navigate to="/" />;
+    }
 }
 
 export default Posts;
